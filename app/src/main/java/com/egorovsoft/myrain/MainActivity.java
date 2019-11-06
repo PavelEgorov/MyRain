8f7b54b7a8e483a39f5f56ec9f660a67779a79dc
PavelEgorov <jonnycomein@gmail.com>
2019-11-06 23:17:10 +0300

    Урок 2. Добавил активити с выбором города и настройками. Поставил активити с выбором города при старте. Сделал открытие основной активити при выборе города. Картинки пока убрал (спасибо за комментарий). На выходных ещё раз обдумаю дизайн и попробую все сделать правильно.

--- app/src/main/java/com/egorovsoft/myrain/MainActivity.java
+++ app/src/main/java/com/egorovsoft/myrain/MainActivity.java
@@ -2,13 +2,48 @@ package com.egorovsoft.myrain;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        /// Включаем чтобы стартавала страница с выбором города
+        setContentView(R.layout.activity_city_change);
+
+        findViewById(R.id.buttonKrasnodar).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                buttonClick("Krasnodar");
+            }
+        });
+        findViewById(R.id.buttonMoscow).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                buttonClick("Moscow");
+            }
+        });
+            findViewById(R.id.buttonNovosibirsk).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                buttonClick("Novosibirsk");
+            }
+        });
+        findViewById(R.id.buttonPetersburg).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                buttonClick("Petersburg");
+            }
+        });
+    }
+
+    protected void buttonClick(String msg){
+        // Активити создавал вручную, по этому не переключается, возможно что-то не так сделал.
+        // Пока закомментировал код. В будущем перепишу.
+        Intent intent = new Intent(MainActivity.this, Main.class);
+        startActivity(intent);
     }
 }
